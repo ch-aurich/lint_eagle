fccbfe689c66f3edc54039be3b560f4a1f149ff3
Christian Aurich (I2SE GmbH) <christian.aurich@i2se.com>
2014-10-01 16:20:39 +0200

    improving failure handling if library package does not contain any text at all (expect >NAME and >VALUE here)

--- lint_eagle/eagle_lbr.py
+++ lint_eagle/eagle_lbr.py
@@ -16,22 +16,24 @@ def lbr_check_name_and_value_package(eagle_object, settings):
 
     name = package.get('name')
 
-    for text in package["text"]:
-        if text == ">VALUE":
-          value_found = True
-          value_layer = text.get('layer')
-        if text == ">NAME":
-          name_found = True
-          name_layer = text.get('layer')
+    text = package.find("text")
+    if text is not None:
+      for text in package["text"]:
+          if text == ">VALUE":
+            value_found = True
+            value_layer = text.get('layer')
+          if text == ">NAME":
+            name_found = True
+            name_layer = text.get('layer')
 
     if (value_found == False):
       issues.append(lint_framework.lint_issue("E", 2, "package " + name, "\">VALUE\" could not be found"))
     if (name_found == False):
       issues.append(lint_framework.lint_issue("E", 1, "package " + name, "\">NAME\" could not be found"))
 
-    if (value_found and not value_layer == 27):
+    if (value_found and not int(value_layer) == 27):
       issues.append(lint_framework.lint_issue("E", 7, "package " + name, "\">VALUE\" is not on layer 27, tValues"))
-    if (name_found and not name_layer == 25):
+    if (name_found and not int(name_layer) == 25):
       issues.append(lint_framework.lint_issue("E", 5, "package " + name, "\">NAME\" is not on layer 25, tNames"))
       
     #TODO: check for correct layers; does this need to be done based on the layer definitions in the file?
